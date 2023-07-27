63f318f17bd82fb845cc3e997f5c322505e33506
Manuel Crovetto <87724520+ManuelCrovetto@users.noreply.github.com>
2023-07-27 16:20:36 +0200

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className={"m-auto min-w-[300] max-w-7xl p-4"}>{children}</main>
+      </body>
     </html>
   )
 }
